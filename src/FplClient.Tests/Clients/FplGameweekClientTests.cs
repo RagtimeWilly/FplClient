15286f4b3178eb9d92ff9769bcbbf0ad6e9230f7
RagtimeWilly <sean.comiskey@gmail.com>
2017-08-14 13:25:43 +1000

    Added FplGameweek client

---
+++ src/FplClient.Tests/Clients/FplGameweekClientTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using FplClient.Clients;
+using NUnit.Framework;
+
+namespace FplClient.Tests.Clients
+{
+    [TestFixture]
+    public class FplGameweekClientTests
+    {
+        private TestContext _context;
+
+        [SetUp]
+        public void Setup()
+        {
+            _context = new TestContext();
+        }
+
+        [Test]
+        public void Retrieves_gameweeks_successfully()
+        {
+            var data = _context.Sut.GetGameweeks().Result;
+
+            Assert.AreEqual(38, data.Count());
+        }
+
+        private class TestContext
+        {
+            public TestContext()
+            {
+                Sut = new FplGameweekClient(() => new HttpClient());
+            }
+
+            public FplGameweekClient Sut { get; }
+        }
+    }
+}
---
+++ src/FplClient/Clients/FplGameweekClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FplClient.Data;
+using Newtonsoft.Json;
+
+namespace FplClient.Clients
+{
+    public class FplGameweekClient : IFplGameweekClient
+    {
+        private readonly Func<HttpClient> _clientFactory;
+
+        public FplGameweekClient(Func<HttpClient> clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<IEnumerable<FplGameweek>> GetGameweeks()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            using (var client = _clientFactory())
+            {
+                const string url = "https://fantasy.premierleague.com/drf/events";
+
+                var json = await client.GetStringAsync(url);
+
+                return JsonConvert.DeserializeObject<IEnumerable<FplGameweek>>(json);
+            }
+        }
+    }
+}
